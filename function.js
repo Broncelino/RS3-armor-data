2ae5e1fb55f1fc9a7533e42e702212296a58015e
Broncelino <bsojn27@gmail.com>
2022-06-27 00:55:51 -0600

    added javascript to get stats from json

---
+++ function.js
@@ -0,0 +1,105 @@
+function dataGet() {
+    fetch('./data.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        /*for(var i=0; i<data.length; i++) {
+            document.getElementById("data").innerHTML +=
+            data[0].bronze.defence + " " + data[1].VirtusArmor.ring_id + " " + data[0].bronze.damage + "<br />";
+        }*/
+        document.getElementById("data").innerHTML +=
+        data[0].bronze.defence + " " + data[1].VirtusArmor.ring_id + " " + data[0].bronze.damage + "<br />";
+    })
+    .catch(function (err) {
+        console.log(err);
+    });}
+
+    function getStats() {
+        getDefence(); getHealth(); getRange(); getPrayer(); getStrength(); getMagic();
+    }
+
+    function getDefence() {
+        var x = document.getElementById("table");
+        var y = document.getElementById("armor");
+        fetch('./data.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            document.getElementById("defence").innerHTML =
+            data[x.value][y.value].def + "<br />";
+        })
+        .catch(function (err) {
+            console.log(err);
+        });}
+    function getHealth() {
+        var x = document.getElementById("table");
+        var y = document.getElementById("armor");
+        fetch('./data.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            document.getElementById("health").innerHTML =
+            data[x.value][y.value].health + "<br />";
+        })
+        .catch(function (err) {
+            console.log(err);
+        });}
+    function getPrayer() {
+        var x = document.getElementById("table");
+        var y = document.getElementById("armor");
+        fetch('./data.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            document.getElementById("prayer").innerHTML =
+            data[x.value][y.value].prayer + "<br />";
+        })
+        .catch(function (err) {
+            console.log(err);
+        });}
+    function getRange() {
+        var x = document.getElementById("table");
+        var y = document.getElementById("armor");
+        fetch('./data.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            document.getElementById("range").innerHTML =
+            data[x.value][y.value].range + "<br />";
+        })
+        .catch(function (err) {
+            console.log(err);
+        });}
+    function getStrength() {
+        var x = document.getElementById("table");
+        var y = document.getElementById("armor");
+        fetch('./data.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            document.getElementById("strength").innerHTML =
+            data[x.value][y.value].strength + "<br />";
+        })
+        .catch(function (err) {
+            console.log(err);
+        });}
+    function getMagic() {
+        var x = document.getElementById("table");
+        var y = document.getElementById("armor");
+        fetch('./data.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            document.getElementById("magic").innerHTML =
+            data[x.value][y.value].magic + "<br />";
+        })
+        .catch(function (err) {
+            console.log(err);
+        });}
