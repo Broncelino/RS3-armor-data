804a2daab6efe2838e25f774d0ea0a542579a5d1
Broncelino <bsojn27@gmail.com>
2022-06-27 12:36:54 -0600

    add picture getting to function made funtions into 1

--- function.js
+++ function.js
@@ -1,25 +1,25 @@
-function dataGet() {
+/*function dataGet() {
     fetch('./data.json')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        /*for(var i=0; i<data.length; i++) {
+        for(var i=0; i<data.length; i++) {
             document.getElementById("data").innerHTML +=
             data[0].bronze.defence + " " + data[1].VirtusArmor.ring_id + " " + data[0].bronze.damage + "<br />";
-        }*/
+        }
         document.getElementById("data").innerHTML +=
         data[0].bronze.defence + " " + data[1].VirtusArmor.ring_id + " " + data[0].bronze.damage + "<br />";
     })
     .catch(function (err) {
         console.log(err);
-    });}
+    });}*/
+
+
+
+
 
     function getStats() {
-        getDefence(); getHealth(); getRange(); getPrayer(); getStrength(); getMagic();
-    }
-
-    function getDefence() {
         var x = document.getElementById("table");
         var y = document.getElementById("armor");
         fetch('./data.json')
@@ -29,11 +29,31 @@ function dataGet() {
         .then(function (data) {
             document.getElementById("defence").innerHTML =
             data[x.value][y.value].def + "<br />";
+
+            document.getElementById("health").innerHTML =
+            data[x.value][y.value].health + "<br />";
+
+            document.getElementById("prayer").innerHTML =
+            data[x.value][y.value].prayer + "<br />";
+
+            document.getElementById("range").innerHTML =
+            data[x.value][y.value].range + "<br />";
+
+            document.getElementById("strength").innerHTML =
+            data[x.value][y.value].strength + "<br />";
+
+            document.getElementById("magic").innerHTML =
+            data[x.value][y.value].magic + "<br />";
+            // virtusMask.png
+            document.getElementById('equip').src = data[x.value][y.value].imagePath; //"./images/virtusMask.png"
         })
         .catch(function (err) {
             console.log(err);
         });}
-    function getHealth() {
+
+
+
+    /*function getHealth() {
         var x = document.getElementById("table");
         var y = document.getElementById("armor");
         fetch('./data.json')
@@ -102,4 +122,5 @@ function dataGet() {
         })
         .catch(function (err) {
             console.log(err);
-        });}
+        });}*/
+        
